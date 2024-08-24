c212b6ce942c171f08bc6e6f80f70a508457e02b
RAMOHAM <reda.ali.moham@gmail.com>
2024-08-24 19:05:31 +0100

    initial

---
+++ src/main/java/com/example/carplatserver/CarPlatServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.carplatserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarPlatServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarPlatServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/carplatserver/CarPlatServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carplatserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarPlatServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
